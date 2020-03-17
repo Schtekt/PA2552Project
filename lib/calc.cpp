4cb1c5482e7115061a17f668d83069abdce467c9
Tim Johansson <totimom@live.se>
2020-03-17 11:03:06 +0100

    Added function declarations in for calculator.

--- lib/calc.cpp
+++ lib/calc.cpp
@@ -10,7 +10,40 @@ float Calc::Addition(float valOne, float valTwo)
 	return m_currVal;
 }
 
+float Calc::Subtraction(float valOne, float valTwo)
+{
+	return 0.0f;
+}
+
+float Calc::Multiplication(float valOne, float valTwo)
+{
+	return 0.0f;
+}
+
+float Calc::Division(float valOne, float valTwo)
+{
+	return 0.0f;
+}
+
 float Calc::GetVal() const
 {
 	return m_currVal;
+}
+
+float Calc::MemAdd()
+{
+	return 0.0f;
+}
+
+float Calc::GetMem() const
+{
+	return 0.0f;
+}
+
+void Calc::Reset()
+{
+}
+
+void Calc::MemReset()
+{
 }
